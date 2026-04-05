f2f469d1d298ff30e06ba6c1449448e712f095c1
ritikyawalkar111 <ritikyawalkar21@gmail.com>
2026-04-05 21:20:19 +0530

    learned dataypes conversion

---
+++ basic/03datatypeconversion.js
@@ -0,0 +1,15 @@
+let score="33"
+let value=Number(score)
+let value2=String(value);
+console.log(score,value,value2);
+console.table([score,value,value2]);
+console.log(Number(undefined))//nan
+console.log(Number(null))//0
+console.log(Boolean(null))//false
+console.log(Boolean(0))//false
+console.log(Boolean(" "))//true
+console.log(Boolean(1))//true
+console.log(Boolean(""))//empty string equals false
+
+//checking type of variable using typeof()
+
